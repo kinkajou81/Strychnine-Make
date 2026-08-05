03d56c33c29149888a8bb355f14b4f10b6341ef4
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-04 21:27:20 -0400

    Feat(main): Added argument parsing via sys.argv

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ import os
 from threading import Thread
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
+import sys
 
 def main():
+    flags = [x for x in sys.argv[1:] if (x[0] == "-")]
+    args = [x for x in sys.argv[1:] if (x[0] != "-")]
+
     return 0
 
 errcode = main()
