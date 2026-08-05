7b3c8495fb48a3a61083085dac2f1d020d10e23e
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-04 21:00:15 -0400

    Feat!: Create new entry point main.py
    New entry point created to allow for a clean rewrite

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import os
+from threading import Thread
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+def main():
+    return 0
+
+errcode = main()
+if(errcode != 0):
+    print("Exiting With Failure...")
+else:
+    print("Exiting...")
