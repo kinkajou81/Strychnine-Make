7dd556b1e3dcf319e5f1cd65186c9b7c128b3423
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-04 22:21:48 -0400

    Feat(parser.py): Added function find_mode_bounds

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 from typing import List
+import string_editing as se
 
 def get_modes(list_strings: List[str]) -> List[str]:
     out: List[str] = []
@@ -6,4 +7,19 @@ def get_modes(list_strings: List[str]) -> List[str]:
     while(list_strings[line][:1] != "??"):
         out.append(list_strings[line])
         line += 1
+    return out
+
+def find_mode_bounds(mode: str, list_strings: List[str]) -> List[int]:
+    out: List[int] = [0,0]
+    target: str = "??>" + mode
+
+    line: int = 0 # two loops with shared index to ensure proper ordering
+    while(line < len(list_strings)):
+        if(se.remove_whitespace(list_strings[line]) == target):
+            out[0] = line
+            break
+    while(line < len(list_strings)):
+        if(se.remove_whitespace(list_strings[line]) == "??."):
+            out[1] = line
+            break
     return out
