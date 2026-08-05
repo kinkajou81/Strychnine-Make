32fe559aa4099e29d0535496174017cd2ec3f6cc
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-04 21:32:29 -0400

    Feat(main): Added settings file access
    - Added settings file access
    - Named settings file strymake.conf

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ def main():
     flags = [x for x in sys.argv[1:] if (x[0] == "-")]
     args = [x for x in sys.argv[1:] if (x[0] != "-")]
 
+    settings_file = open("strymake.conf", "r")
+    settings_source: str = settings_file.read()
+    settings_file.close()
+
     return 0
 
 errcode = main()
