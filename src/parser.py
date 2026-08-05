cba3b940115ac28a801e7a61a7a4087fb18186c5
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-05 00:43:21 -0400

    Fix(parser.py): Added proper array index check to get_modes to prevent out of bounds access

--- src/parser.py
+++ src/parser.py
@@ -7,7 +7,7 @@ import sys
 def get_modes(list_strings: List[str]) -> List[str]:
     out: List[str] = []
     line: int = 0
-    while(list_strings[line][:2] != "??"):
+    while((list_strings[line][:2] != "??") and (line < len(list_strings))):
         out.append(list_strings[line])
         line += 1
     if(out == []):
