e7aabf88ca95c51e8c50ef4509e3ee9f3ae2f610
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-19 18:35:14 -0400

    Feat(filesystem): Added functions to list subdirectories

---
+++ src/filesystem.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+def list_subdirectories(path: str):
+    if(not os.path.isdir(path)):
+        print("ERROR: Invalid directory: " + path + "\n")
+        sys.exit(1)
+
+    with os.scandir(path) as it:
+        return [
+            entry.path
+            for entry in it
+            if entry.is_dir() and not entry.name.startswith(".")
+        ]
+        
+
+def list_subdirectories_ex(path: str, recursive: bool):
+    if(not recursive):
+        return list_subdirectories(path)
+
+    if(not os.path.isdir(path)):
+        print("ERROR: Invalid directory: " + path + "\n")
+        sys.exit(1)
+
+    out = []
+    for root, directories, _ in os.walk(path, followlinks=True):
+        directories[:] = [d for d in directories if not d.startswith(".")]
+        out.extend(os.path.join(root, d) for d in directories)
+    return out
