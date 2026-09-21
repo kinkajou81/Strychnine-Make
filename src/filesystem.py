b2be8037fc160acbbbb5aff31f400c63163c6c74
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-20 23:02:52 -0400

    Feat(filesystem): Added function make_build_directory

--- src/filesystem.py
+++ src/filesystem.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from pathlib import Path
 
 def list_subdirectories(path: str):
     if(not os.path.isdir(path)):
@@ -27,3 +28,16 @@ def list_subdirectories_ex(path: str, recursive: bool):
         directories[:] = [d for d in directories if not d.startswith(".")]
         out.extend(os.path.join(root, d) for d in directories)
     return out
+
+def make_build_directory(path: str):
+    old_path = os.getcwd()
+
+    os.chdir(path)
+    os.mkdir(".strychnine_make")
+    os.chdir(".strychnine_make")
+    Path("data.txt").touch()
+
+    os.chdir(old_path)
+
+def make_build_directories(paths):
+    
